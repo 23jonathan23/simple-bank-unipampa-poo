81792d2b5bb1c4764c61d55995b8f5270b4ef17b
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-04-29 22:22:23 -0300

    Interface removed

--- src/Domain/Account.java
+++ src/Domain/Account.java
@@ -4,64 +4,62 @@ import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
 import java.util.List;
 
-import src.Domain.Interfaces.IAccount;
-
-public class Account implements IAccount {
-    protected static int _totalAccounts = 0;
-    protected final int INITIAL_ACCOUNT_NUMBER = 1000;
+public class Account {
+	protected static int _totalAccounts = 0;
+	protected final int INITIAL_ACCOUNT_NUMBER = 1000;
 	protected final int _number;
 	protected String _holder;
 	protected double _balance = 0;
-    protected List<Transaction> _transactions = new ArrayList<Transaction>();
+	protected List<Transaction> _transactions = new ArrayList<Transaction>();
 
-    public Account(String holder) {
+	public Account(String holder) {
 		_number = INITIAL_ACCOUNT_NUMBER + _totalAccounts;
 		_holder = holder;
-		
-        Account._totalAccounts++;
+
+		Account._totalAccounts++;
 	}
 
-    public int getNumber() {
+	public int getNumber() {
 		return _number;
 	}
-	
+
 	public String getHolder() {
 		return _holder;
 	}
-	
+
 	public double getBalance() {
 		return _balance;
 	}
 
-    public void deposit(Transaction transaction) throws IllegalArgumentException {
-        if(transaction.getAmount() <= 0) {
-            throw new IllegalArgumentException("Valor fornecido é inválido!");
-        }
+	public void deposit(Transaction transaction) throws IllegalArgumentException {
+		if (transaction.getAmount() <= 0) {
+			throw new IllegalArgumentException("Valor fornecido é inválido!");
+		}
 
 		_balance += transaction.getAmount();
-        _transactions.add(transaction);
+		_transactions.add(transaction);
 	}
-	
+
 	public void withdraw(Transaction transaction) throws IllegalArgumentException {
-		if(transaction.getAmount() > _balance) { 
-            throw new IllegalArgumentException("Saldo insuficiente!");
-        }
+		if (transaction.getAmount() > _balance) {
+			throw new IllegalArgumentException("Saldo insuficiente!");
+		}
 
-        _balance -= transaction.getAmount();
-        _transactions.add(transaction);
+		_balance -= transaction.getAmount();
+		_transactions.add(transaction);
 	}
 
-    public String extract() {
-        var extract = toString() + "Transações realizadas: \n";
+	public String extract() {
+		var extract = toString() + "Transações realizadas: \n";
 
-        for(var transaction : _transactions){
+		for (var transaction : _transactions) {
 			extract += transaction.toString();
 		}
 
-        return extract;
-    }
-	
+		return extract;
+	}
+
 	public String toString() {
-		return "Numero: " + _number + "\nTitular: " + _holder + "\nSaldo: "+ _balance + "\n";
+		return "Numero: " + _number + "\nTitular: " + _holder + "\nSaldo: " + _balance + "\n";
 	}
 }
--- src/Domain/Interfaces/IAccount.java
+++
@@ -1,9 +0,0 @@
-package src.Domain.Interfaces;
-
-import src.Domain.Transaction;
-
-public interface IAccount {
-    void deposit(Transaction transaction) throws IllegalArgumentException;
-    void withdraw(Transaction transaction) throws IllegalArgumentException;
-    String extract();
-}
